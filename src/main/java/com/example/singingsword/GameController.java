b6cb8b206a55789a3ad3b66d8341977a664a378b
EndRay <a.i.kovrigin@gmail.com>
2022-11-11 21:10:42 +0100

    canvas added background

--- src/main/java/com/example/singingsword/GameController.java
+++ src/main/java/com/example/singingsword/GameController.java
@@ -1,24 +1,40 @@
 package com.example.singingsword;
 
 import com.example.singingsword.sound.PitchExtractor;
+import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.FloatProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleFloatProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 import javax.sound.sampled.*;
 
 import static com.example.singingsword.sound.Sound.SAMPLE_RATE;
+import static java.lang.Math.*;
 
-public class HelloController {
+public class GameController {
     @FXML
-    private Label welcomeText;
+    private Canvas canvas;
+
+    private static final Image swordImage = new Image(GameController.class.getResource("images/sword.png").toString(), 128, 128, true, false);
+    private static final Image enemyImage = new Image(GameController.class.getResource("images/enemy.png").toString(), 128, 128, true, false);
+    private static final Image backgroundImage = new Image(GameController.class.getResource("images/background.png").toString(), 960, 720, true, false);
+
+    private static final float backgroundMovingSpeed = 40f;
+
+
+    final private float minFrequency = 100;
+    final private float maxFrequency = 800;
 
     final private FloatProperty frequencyProperty = new SimpleFloatProperty();
     final private BooleanProperty isSingingProperty = new SimpleBooleanProperty();
+
+    final private FloatProperty swordPositionProperty = new SimpleFloatProperty();
 
     public void initialize() {
         Thread soundParsingThread = new Thread(() -> {
@@ -54,23 +70,27 @@ public class HelloController {
         soundParsingThread.setDaemon(true);
         soundParsingThread.start();
 
-        isSingingProperty.addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                welcomeText.setText("o={=====>");
-            } else {
-                welcomeText.setText(".______.");
-                welcomeText.setTranslateY(0);
-            }
-        });
-        frequencyProperty.addListener((observable, oldValue, newValue) -> {
-            if (isSingingProperty.get()) {
-                welcomeText.setTranslateY(-(newValue.floatValue() - 120) / 5);
-            }
-        });
-    }
+        frequencyProperty.addListener(((observable, oldValue, newValue) -> {
+            float value = (float) (log(newValue.floatValue() / minFrequency) / log(maxFrequency / minFrequency));
+            value = max(0, min(1, value));
+            swordPositionProperty.set(value);
+        }));
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        final long startNanoTime = System.nanoTime();
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                float t = (now - startNanoTime) / 1000000000f;
+                float backgroundPos = (t * backgroundMovingSpeed)%((float)canvas.getWidth());
+                gc.drawImage(backgroundImage, -backgroundPos, 0);
+                gc.drawImage(backgroundImage, -backgroundPos + canvas.getWidth(), 0);
+                gc.drawImage(swordImage, 0, (1-swordPositionProperty.floatValue()) * (canvas.getHeight() - swordImage.getHeight()));
+
+            }
+        };
+        timer.start();
     }
 }
--- src/main/java/com/example/singingsword/HelloApplication.java
+++ src/main/java/com/example/singingsword/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 960, 720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
